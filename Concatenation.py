9c1e3ff5f512ecd1617d1205c2686eedaa16883d
Alda987 <aldatreesajosy05@gmail.com>
2024-10-20 18:15:07 +0200

    Add files via upload

---
+++ Concatenation.py
@@ -0,0 +1,8 @@
+'''
+Author: ALDA TREESA JOSY 
+Date:18/10/2024
+Discription:stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+string1=("hello,world")
+string2=string1[6:12]
+print(string2 +" "+"everyone!")
