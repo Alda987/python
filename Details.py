76a7e77fc2ab5d3c53eadcf213ee947f37f8f10f
Alda987 <aldatreesajosy05@gmail.com>
2024-10-20 18:13:50 +0200

    Add files via upload

---
+++ Details.py
@@ -0,0 +1,8 @@
+'''
+Author:ALDA TREESA JOSY 
+Date:18/10/2024
+Discription:the user to input their name and age. After receiving the input, display a message that includes the user's name and age
+'''
+Name=input("enter your name:")
+Age=input("enter your age:")
+print("hello",Name,"!you are",Age," years old")
