20705b3e145f373c4b857b0f8fbe62b913160361
Alda987 <aldatreesajosy05@gmail.com>
2024-10-20 18:14:24 +0200

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,13 @@
+'''
+Author: ALDA TREESA JOSY 
+Date:18/10/2024
+Description:uses functions from the math module to perform the following operations on a number provided by the user:
+
+Find the square root.
+Calculate the factorial.
+Raise the number to the power of 2.
+'''
+import math
+number=int(input("Enter a number:"))
+print("square root of",number,math.sqrt(number))
+print("factorial of",number,math.factorial(number))
