6e3d479807aed6410543939ce381e4518a8f9e82
Alda987 <aldatreesajosy05@gmail.com>
2024-10-20 18:16:27 +0200

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,13 @@
+'''
+Author: ALDA TREESA JOSY 
+Date:18/10/2024
+Description:demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results
+'''
+a=int(input("enter your name:"))
+b=int(input("enter your name:"))
+print("sum",a+b)
+print("division",a/b)
+print("is a greater than b?",a>b)
+print("are a and b equal?",a==b)
+print("logical AND",a>b and b<a)
+print("logical OR",a<b or b>a)
