c9391dbd3af1452284c6c9df9bb9fc447b71327a
Alda987 <aldatreesajosy05@gmail.com>
2024-10-25 16:00:01 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR: ALDA TREESA JOSY
+DATE:25/10/2024
+DESCRIPTION:Write a Python program to convert temperature values back and forth between Celsius and Fahrenheit. The user should be able to input a temperature in Celsius or Fahrenheit, and the program should convert it to the other unit
+'''
+num1=int(input("enter a number"))
+if num1>0:
+  print("num1 is positive")
+else:
+ print("num1 is negative")
